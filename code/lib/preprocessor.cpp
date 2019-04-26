d3dcbcb217fe56a71de666b89a3c67c8d730ceaa
Abdullah Zaiter <abdullah.zaiter@hotmail.com>
2019-04-26 01:20:32 -0300

    preprocessor special characters treatment done and tested

--- code/lib/preprocessor.cpp
+++ code/lib/preprocessor.cpp
@@ -22,14 +22,59 @@ void PreProcessor::printFile(std::fstream *source)
     source->seekp( 0, source->beg);
 }
 
+std::vector<uint16_t> PreProcessor::findCharInStringIndexes(std::string sample, char character_to_find)
+{
+    std::vector<uint16_t> character_indexes;
+    for(uint16_t i =0; i < sample.size(); i++)
+    {
+        if(sample[i] == character_to_find)
+        {  
+            character_indexes.push_back(i);
+        }
+    }
+    return character_indexes;
+}
 
+std::vector<std::string> PreProcessor::splitString(std::string word)
+{
+    std::vector<std::string> splitted_words;
+    std::vector<char> special_characters{SPECIAL_CHARACTERS};
+    std::vector<uint16_t> splitting_indexes;
+    splitting_indexes.push_back(0);
+    for(uint16_t i = 0 ; i<=special_characters.size(); i++)
+    {
+        auto aux = findCharInStringIndexes(word,special_characters[i]);
+        splitting_indexes.insert(splitting_indexes.end(),aux.begin(),aux.end());
+    }
+    std::sort(splitting_indexes.begin(), splitting_indexes.end());
+    uint16_t last_index = 0;
+    for (uint16_t i = 1; i < splitting_indexes.size(); i++)
+    {
+        auto aux = word.substr(last_index,splitting_indexes[i]-last_index);
+        splitted_words.push_back(aux);
+        splitted_words.push_back(std::string(1,word[splitting_indexes[i]]));
+        last_index = i;
+    }
+    //this condition treats the cases where a word starts with a special character, pushes the rest of the string as a word to the splitted words
+    if((splitting_indexes.back()) != (word.length()-1) && splitting_indexes.size() > 1)
+    {
+        auto aux = word.substr(splitting_indexes.back()+1,word.length()-1-splitting_indexes.back());
+        splitted_words.push_back(aux);
+    }
+    //this condition treats the case where theres no any special characters in the string
+    if(splitting_indexes.size() == 1 && splitting_indexes[0] == 0 )
+    {
+        splitted_words.push_back(word);
+    }
+
+    return splitted_words;
+}
 
 tuple_list  PreProcessor::removeEmptySpacesAndLines(void)
 { 
     std::string text_line, word;
     std::vector <std::string> sentence;
     std::tuple<uint16_t,std::vector <std::string>> current_tuple;
-    std::string aux;
     tuple_list file_tuple;
     uint16_t line_cont = 0;
     while (std::getline(*(this->source_code_file), text_line))
@@ -38,27 +83,8 @@ tuple_list  PreProcessor::removeEmptySpacesAndLines(void)
         while (text_stream >> word)
         {
             std::transform(word.begin(), word.end(),word.begin(), ::toupper);
-            if (word.back() == ',' || word.back() == ':')
-            {
-                aux = word.back();
-                word.erase(word.end()-1);
-                if(!(word.empty()))
-                {
-                    sentence.push_back(word);
-                }
-                sentence.push_back(aux);
-            }
-            else if (word[0] == ',' || word[0] == ':')
-            {
-                aux = word[0];
-                sentence.push_back(aux);
-                word.erase(word.begin());
-                sentence.push_back(word);
-            }
-            else
-            {
-                sentence.push_back(word);
-            }
+            auto aux = splitString(word);
+            sentence.insert(sentence.end(),aux.begin(),aux.end());
         }
         if(!(text_line.empty()))
         {
@@ -73,10 +99,10 @@ tuple_list  PreProcessor::removeEmptySpacesAndLines(void)
 
 void PreProcessor::printTupleListFile(tuple_list tuple_file)
 {
-    for(uint16_t i = 0; i != tuple_file.size(); i++ )
+    for(size_t i = 0; i != tuple_file.size(); i++ )
     {
         std::cout << std::setfill('0') << std::setw(3) << std::get<0>(tuple_file[i])+1 << " ";
-        for(uint16_t j = 0; j != std::get<1>(tuple_file[i]).size(); j++ )
+        for(size_t j = 0; j != std::get<1>(tuple_file[i]).size(); j++ )
             std::cout << std::get<1>(tuple_file[i])[j] << " ";
         std::cout << std::endl;
     } 
